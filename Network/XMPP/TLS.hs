9107b5a4119d857e5982f9421796f7ec1b3ec513
Jon Kristensen <info@jonkristensen.com>
2011-07-12 23:57:15 +0200

    started using the latest version of the tls package

--- Network/XMPP/TLS.hs
+++ Network/XMPP/TLS.hs
@@ -17,31 +17,29 @@ getTLSParams,
 handshake'
 ) where
 
+import Crypto.Random (newGenIO, SystemRandom)
 import Network.TLS
+import Network.TLS.Extra (cipher_AES128_SHA1)
 import Network.TLS.Cipher
 import GHC.IO.Handle (Handle, hPutStr, hFlush, hSetBuffering, hWaitForInput)
 
 
 getTLSParams :: TLSParams
 getTLSParams = TLSParams { pConnectVersion    = TLS10
-                    , pAllowedVersions   = [TLS10,TLS11]
-                    , pCiphers           = [cipher_AES256_SHA1] -- Check the rest
-                    , pCompressions      = [nullCompression]
-                    , pWantClientCert    = False
-                    , pCertificates      = []
-                    , onCertificatesRecv = \_ -> return True } -- Verify cert chain
+                         , pAllowedVersions   = [TLS10,TLS11]
+                         , pCiphers           = [cipher_AES128_SHA1] -- Check the rest
+                         , pCompressions      = [nullCompression]
+                         , pWantClientCert    = False
+                         , pUseSecureRenegotiation = False -- TODO: No renegotiation
+                         , pCertificates      = []
+                         , pLogging           = defaultLogging
+                         , onCertificatesRecv = \_ -> return CertificateUsageAccept } -- Verify cert chain
 
 handshake' :: Handle -> String -> IO (Maybe TLSCtx)
 handshake' h s = do
   let t = getTLSParams
-  r <- makeSRandomGen
-  case r of
-    Right sr -> do
-      putStrLn $ show sr
-      c <- client t sr h
-      handshake c
-      putStrLn ">>>>TLS data sended<<<<"
-      return (Just c)
-    Left ge -> do
-      putStrLn $ show ge
-      return Nothing
+  r <- newGenIO :: IO SystemRandom -- Investigate limitations
+  c <- client t r h
+  handshake c
+  putStrLn ">>>>TLS data sended<<<<"
+  return (Just c)
