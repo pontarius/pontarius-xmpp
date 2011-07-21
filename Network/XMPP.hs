6c98ccebf7a71841ac0cda78e4946e2388bcd8e2
Jon Kristensen <info@jonkristensen.com>
2011-07-21 23:47:07 +0200

    modified the headers to reflect the new license

--- Network/XMPP.hs
+++ Network/XMPP.hs
@@ -1,29 +1,11 @@
-{-
-
-Copyright © 2010-2011 Jon Kristensen.
-
-This file is part of Pontarius XMPP.
-
-Pontarius XMPP is free software: you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your option) any
-later version.
-
-Pontarius XMPP is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with Pontarius XMPP. If not, see <http://www.gnu.org/licenses/>.
-
--}
+-- Copyright © 2010-2011 Jon Kristensen. See the LICENSE file in the Pontarius
+-- XMPP distribution for more details.
 
 -- |
 -- Module:      $Header$
 -- Description: Pontarius XMPP API
 -- Copyright:   Copyright © 2010-2011 Jon Kristensen
--- License:     LGPL-3
+-- License:     BSD3
 --
 -- Maintainer:  info@pontarius.org
 -- Stability:   unstable
--- Network/XMPP/Utilities.hs
+++ Network/XMPP/Utilities.hs
@@ -1,24 +1,5 @@
-{-
-
-Copyright © 2010-2011 Jon Kristensen.
-
-This file is part of Pontarius XMPP.
-
-Pontarius XMPP is free software: you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your option) any
-later version.
-
-Pontarius XMPP is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with Pontarius XMPP. If not, see <http://www.gnu.org/licenses/>.
-
--}
-
+-- Copyright © 2010-2011 Jon Kristensen. See the LICENSE file in the Pontarius
+-- XMPP distribution for more details.
 
 {-# OPTIONS_HADDOCK hide #-}
 
