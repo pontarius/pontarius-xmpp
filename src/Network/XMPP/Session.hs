6b9ca182841502620a80177d4ead775ec5f9694f
Jon Kristensen <jon.kristensen@nejla.com>
2012-05-09 13:12:29 +0200

    minor formatting and documentation additions

--- src/Network/XMPP/Session.hs
+++ src/Network/XMPP/Session.hs
@@ -10,11 +10,10 @@ import Network.XMPP.Pickle
 import Network.XMPP.Types
 import Network.XMPP.Concurrent
 
-
 sessionXML :: Element
 sessionXML = pickleElem
-                (xpElemBlank "{urn:ietf:params:xml:ns:xmpp-session}session" )
-                ()
+    (xpElemBlank "{urn:ietf:params:xml:ns:xmpp-session}session")
+    ()
 
 sessionIQ :: Stanza
 sessionIQ = IQRequestS $ IQRequest { iqRequestID      = "sess"
@@ -25,6 +24,8 @@ sessionIQ = IQRequestS $ IQRequest { iqRequestID      = "sess"
                                    , iqRequestPayload = sessionXML
                                    }
 
+-- Sends the session IQ set element and waits for an answer. Throws an error if
+-- if an IQ error stanza is returned from the server.
 xmppStartSession :: XMPPConMonad ()
 xmppStartSession = do
     answer <- xmppSendIQ' "session" Nothing Set Nothing sessionXML
@@ -32,7 +33,8 @@ xmppStartSession = do
         Left e -> error $ show e
         Right _ -> return ()
 
-
+-- Sends the session IQ set element and waits for an answer. Throws an error if
+-- if an IQ error stanza is returned from the server.
 startSession :: XMPP ()
 startSession = do
     answer <- sendIQ' Nothing Set Nothing sessionXML
