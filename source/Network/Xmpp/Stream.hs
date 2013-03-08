f8268d72d6d4349697ed10de3b4f587ceb1ed45c
Philipp Balzarek <p.balzarek@googlemail.com>
2013-03-08 13:06:25 +0100

    fix pullStanza not saving conduit state
    Was using withStream' instead of withStream

--- source/Network/Xmpp/Stream.hs
+++ source/Network/Xmpp/Stream.hs
@@ -393,7 +393,7 @@ pullUnpickle p = do
 
 -- | Pulls a stanza (or stream error) from the stream.
 pullStanza :: TMVar Stream -> IO (Either XmppFailure Stanza)
-pullStanza = withStream' $ do
+pullStanza = withStream $ do
     res <- pullUnpickle xpStreamStanza
     case res of
         Left e -> return $ Left e
