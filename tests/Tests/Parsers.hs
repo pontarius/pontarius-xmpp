bcd49254302eda6401ba2e4d369df76bed56c513
Philipp Balzarek <p.balzarek@googlemail.com>
2015-01-01 23:36:59 +0100

    update Parser tests

--- tests/Tests/Parsers.hs
+++ tests/Tests/Parsers.hs
@@ -9,13 +9,13 @@ import Network.Xmpp.Internal
 import Test.Hspec
 import Test.Tasty.QuickCheck
 import Test.Tasty
-import Test.Tasty.Hspec
 import Test.Tasty.TH
+import Test.Tasty.HUnit
 
 import Tests.Arbitrary ()
 
-case_JidFromText :: Spec
-case_JidFromText = describe "jidFromText" $ do
+case_JidFromText :: Assertion
+case_JidFromText = hspec . describe "jidFromText" $ do
     it "parses a full JID" $ jidFromText "foo@bar.com/quux"
                              `shouldBe` Just (Jid (Just "foo")
                                           "bar.com"
@@ -49,8 +49,8 @@ prop_jidFromText_leftInverse :: Jid -> Bool
 prop_jidFromText_leftInverse jid = (jidFromText $ jidToText jid) == Just jid
 
 
-case_LangTagParser :: Spec
-case_LangTagParser = describe "langTagFromText" $
+case_LangTagParser :: Assertion
+case_LangTagParser = hspec . describe "langTagFromText" $
                     it "has some properties" $ pendingWith "Check requirements"
 
 parserTests :: TestTree
